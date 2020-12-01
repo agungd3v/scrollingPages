7090b3e4d739e924b00c65c8c2cb17eb5db400d7
agungd3v <agungd3v@gmail.com>
2020-12-01 07:12:30 +0700

    Initial Commit

---
+++ js/scrollingPages.js
@@ -0,0 +1,96 @@
+class ScrollPages {
+  constructor({ section, navigation = true }) {
+    try {
+      if (section === undefined) {
+        console.error('Oops, maybe you forgot to mark the part that is needed :(')
+        return false
+      }
+      this.scrollView(section, navigation)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  scrollView(section, navigation) {
+    let eLto = 0
+    const sections = document.querySelectorAll(section)
+    sections[0].parentElement.classList.add('h-screen', 'overflow-hidden')
+    if (navigation === true) {
+      const crtWrapperNavs = document.createElement('div')
+      crtWrapperNavs.classList.add('fixed', 'nav-container')
+      const crtNavs = document.createElement('ul')
+      crtNavs.classList.add('navs', 'flex', 'flex-col', 'justify-center', 'items-center')
+      crtWrapperNavs.append(crtNavs)
+      sections[0].parentElement.append(crtWrapperNavs)
+      sections.forEach(() => {
+        const navItem = `<li class="nav-item rounded-full"></li>`
+        crtNavs.innerHTML += navItem
+      })
+      const navItem = document.querySelectorAll('.nav-item')
+      navItem[eLto].classList.add('active')
+      navItem.forEach((e, i) => {
+        e.addEventListener('click', function() {
+          navItem[eLto].classList.remove('active')
+          sections[i].scrollIntoView({
+            behavior: 'smooth'
+          })
+          eLto = i
+          navItem[eLto].classList.add('active')
+        })
+      })
+      document.querySelector('html, body').addEventListener('wheel', wheel => {
+        wheel.preventDefault()
+        let wheelY = Math.floor(wheel.deltaY)
+        if (wheelY > 0) {
+          if (eLto === sections.length - 1) {
+            return false
+          } else {
+            navItem[eLto].classList.remove('active')
+            eLto++
+            sections[eLto].scrollIntoView({
+              behavior: 'smooth'
+            })
+            navItem[eLto].classList.add('active')
+          }
+        } else {
+          if (eLto === 0) {
+            return false
+          } else {
+            navItem[eLto].classList.remove('active')
+            eLto--
+            sections[eLto].scrollIntoView({
+              behavior: 'smooth'
+            })
+            navItem[eLto].classList.add('active')
+          }
+        }
+      }, { passive: false })
+    } else if (navigation === false || navigation === undefined) {
+      document.querySelector('html, body').addEventListener('wheel', wheel => {
+        wheel.preventDefault()
+        let wheelY = Math.floor(wheel.deltaY)
+        if (wheelY > 0) {
+          if (eLto === sections.length - 1) {
+            return false
+          } else {
+            eLto++
+            sections[eLto].scrollIntoView({
+              behavior: 'smooth'
+            })
+          }
+        } else {
+          if (eLto === 0) {
+            return false
+          } else {
+            eLto--
+            sections[eLto].scrollIntoView({
+              behavior: 'smooth'
+            })
+          }
+        }
+      }, { passive: false })
+    } else {
+      console.error('Oops. Navigation contains only true or false, the default for navigation is true even if you did not enter it :)')
+      return false
+    }
+  }
+}
